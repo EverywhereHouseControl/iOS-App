febe3ad156906099a018d76b4b767c3ce713070b
KINKI <victorv712@hotmail.com>
2014-01-19 21:49:10 +0100

    Añadida vista Lights 

---
+++ ehc/ehc/Clases/LightsItem/LightItemViewController.h
@@ -0,0 +1,13 @@
+//
+//  LightItemViewController.h
+//  ehc
+//
+//  Created by Víctor Vicente on 19/01/14.
+//  Copyright (c) 2014 EHC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LightItemViewController : UIViewController
+
+@end
---
+++ ehc/ehc/Clases/LightsItem/LightItemViewController.m
@@ -0,0 +1,38 @@
+//
+//  LightItemViewController.m
+//  ehc
+//
+//  Created by Víctor Vicente on 19/01/14.
+//  Copyright (c) 2014 EHC. All rights reserved.
+//
+
+#import "LightItemViewController.h"
+
+@interface LightItemViewController ()
+
+@end
+
+@implementation LightItemViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
