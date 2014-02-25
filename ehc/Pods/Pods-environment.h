dd90bf1520b66d0172db8b5ec4154049a8eba790
KINKI <victorv712@hotmail.com>
2014-02-25 11:55:36 +0100

    Añadido Cocoa Pods, abrir siempre desde workspace

---
+++ ehc/Pods/Pods-environment.h
@@ -0,0 +1,20 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// AFNetworking
+#define COCOAPODS_POD_AVAILABLE_AFNetworking
+#define COCOAPODS_VERSION_MAJOR_AFNetworking 1
+#define COCOAPODS_VERSION_MINOR_AFNetworking 3
+#define COCOAPODS_VERSION_PATCH_AFNetworking 3
+
+// ECSlidingViewController
+#define COCOAPODS_POD_AVAILABLE_ECSlidingViewController
+#define COCOAPODS_VERSION_MAJOR_ECSlidingViewController 1
+#define COCOAPODS_VERSION_MINOR_ECSlidingViewController 3
+#define COCOAPODS_VERSION_PATCH_ECSlidingViewController 3
+
