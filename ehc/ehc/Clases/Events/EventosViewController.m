8f31081651f012756f68171fb4348055da3c6e50
KINKI <victorv712@hotmail.com>
2014-02-28 17:21:38 +0100

    Add submodule CKCalendar for Events

--- ehc/ehc/Clases/Events/EventosViewController.m
+++ ehc/ehc/Clases/Events/EventosViewController.m
@@ -7,8 +7,16 @@
 //
 
 #import "EventosViewController.h"
+#import "CKCalendarView.h"
+#import "IonIcons.h"
 
-@interface EventosViewController ()
+@interface EventosViewController ()<CKCalendarDelegate>
+
+@property(nonatomic, weak) CKCalendarView *calendar;
+@property(nonatomic, strong) UILabel *dateLabel;
+@property(nonatomic, strong) NSDateFormatter *dateFormatter;
+@property(nonatomic, strong) NSDate *minimumDate;
+@property(nonatomic, strong) NSArray *disabledDates;
 
 @end
 
@@ -23,6 +31,43 @@
     return self;
 }
 
+- (void)configureViewCalendar{
+    CKCalendarView *calendar = [[CKCalendarView alloc] initWithStartDay:startMonday];
+    self.calendar = calendar;
+    calendar.delegate = self;
+    self.calendar.backgroundColor = colorApp;
+    self.dateFormatter = [[NSDateFormatter alloc] init];
+    [self.dateFormatter setDateFormat:@"dd/MM/yyyy"];
+    self.minimumDate = [self.dateFormatter dateFromString:@"20/09/2012"];
+    
+    self.disabledDates = @[
+                           [self.dateFormatter dateFromString:@"05/01/2013"],
+                           [self.dateFormatter dateFromString:@"06/01/2013"],
+                           [self.dateFormatter dateFromString:@"07/01/2013"]
+                           ];
+    
+    calendar.onlyShowCurrentMonth = NO;
+    calendar.adaptHeightToNumberOfWeeksInMonth = YES;
+    
+    calendar.frame = CGRectMake(10, 90, 300, 320);
+    [self.view addSubview:calendar];
+    
+    self.dateLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, CGRectGetMaxY(calendar.frame) + 4, self.view.bounds.size.width, 24)];
+    [self.view addSubview:self.dateLabel];
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(localeDidChange) name:NSCurrentLocaleDidChangeNotification object:nil];
+}
+
+- (void)configureNavBar{
+    UIBarButtonItem *addItem = [[UIBarButtonItem alloc] initWithImage:[IonIcons imageWithIcon:icon_ios7_plus_outline size:30.0f color:[UIColor whiteColor]] style:UIBarButtonItemStylePlain target:self action:@selector(addButtonAction)];
+    
+    NSArray *actionButtonItems = @[addItem];
+    self.navigationItem.rightBarButtonItems = actionButtonItems;
+    
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -33,12 +78,70 @@
     [super viewWillAppear:animated];
     
     [self.navigationController.navigationBar setHidden:NO];
+    
+    [self configureViewCalendar];
+    
+    [self configureNavBar];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)localeDidChange {
+    [self.calendar setLocale:[NSLocale currentLocale]];
+}
+
+- (BOOL)dateIsDisabled:(NSDate *)date {
+    for (NSDate *disabledDate in self.disabledDates) {
+        if ([disabledDate isEqualToDate:date]) {
+            return YES;
+        }
+    }
+    return NO;
+}
+
+#pragma mark -
+#pragma mark - CKCalendarDelegate
+
+- (void)calendar:(CKCalendarView *)calendar configureDateItem:(CKDateItem *)dateItem forDate:(NSDate *)date {
+    // TODO: play with the coloring if we want to...
+    if ([self dateIsDisabled:date]) {
+        dateItem.backgroundColor = [UIColor redColor];
+        dateItem.textColor = [UIColor whiteColor];
+    }
+}
+
+- (BOOL)calendar:(CKCalendarView *)calendar willSelectDate:(NSDate *)date {
+    return ![self dateIsDisabled:date];
+}
+
+- (void)calendar:(CKCalendarView *)calendar didSelectDate:(NSDate *)date {
+    self.dateLabel.text = [self.dateFormatter stringFromDate:date];
+}
+
+- (BOOL)calendar:(CKCalendarView *)calendar willChangeToMonth:(NSDate *)date {
+    /*if ([date laterDate:self.minimumDate] == date) {
+        //self.calendar.backgroundColor = [UIColor blueColor];
+        return YES;
+    } else {
+        //self.calendar.backgroundColor = [UIColor redColor];
+        return NO;
+    }*/
+    return YES;
+}
+
+- (void)calendar:(CKCalendarView *)calendar didLayoutInRect:(CGRect)frame {
+    NSLog(@"calendar layout: %@", NSStringFromCGRect(frame));
+}
+
+
+#pragma mark - Methods AddTarea
+
+- (void) addButtonAction{
+    
 }
 
 @end
